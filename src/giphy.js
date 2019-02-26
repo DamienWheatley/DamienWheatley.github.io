a88efb2b51316e1c3e92c3302e727adc3c63b898
Damien Wheatley <damien.wheatley@harrisfederation.org.uk>
2019-02-26 16:19:35 +0000

    Adding GIPHY API

---
+++ src/giphy.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    
+    let request = new XMLHttpRequest;
+
+    request.open('GET', 'http://api.giphy.com/v1/gifs/trending?api_key=sJgDVeZDHq2Gbm47OnWevUpmFaHsHf6c&limit=10')
+
+    request.onload = function () {
+        if (request.status >= 200 && request.status <= 400){
+            data = JSON.parse(request.responseText).data.image_url;
+            console.log(data);
+            document.getElementById("trendingGIFS").innerHTML = '<img src="'+data+'" title="GIF via GIPHY">';
+        } else {
+            console.log('Reached GIPHY but API returned an error');
+        }
+    }
+    request.send();
+});
