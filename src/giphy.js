a5c50a77caba2c0d21d18bb2c3cc6459c9d67661
Damien Wheatley <damien.wheatley@harrisfederation.org.uk>
2019-02-28 10:33:29 +0000

    Adding GIPHY API

--- src/giphy.js
+++ src/giphy.js
@@ -1,24 +1,24 @@
-    document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
 
-        let request = new XMLHttpRequest;
-        request.open('GET', 'https://api.giphy.com/v1/gifs/trending?api_key=sJgDVeZDHq2Gbm47OnWevUpmFaHsHf6c&limit=10')
+    let request = new XMLHttpRequest;
+    request.open('GET', 'https://api.giphy.com/v1/gifs/trending?api_key=sJgDVeZDHq2Gbm47OnWevUpmFaHsHf6c&limit=10')
 
-        request.onload = function () {
-            if (request.status >= 200 && request.status <= 400){
-                console.log(request.status);
-                let data = JSON.parse(request.responseText);
-                console.log(data);
-                let _trendingGIfs = document.getElementById("trendingGIFS");
+    request.onload = function () {
+        if (request.status >= 200 && request.status <= 400){
+            console.log(request.status);
+            let data = JSON.parse(request.responseText);
+            console.log(data);
+            let _trendingGIfs = document.getElementById("trendingGIFS");
 
-                if(data.data.length < 10)return;
-                for(let i=0;i<10;i++){
-                    var node = document.getElementById("gif~"+i);
-                    node.setAttribute("src",data.data[i].images.fixed_width.url);
-                    _trendingGIfs.appendChild(data);
-                }
-            } else {
-                console.log('Reached GIPHY but API returned an error');
+            if(data.data.length < 10)return;
+            for(let i=0;i<10;i++){
+                var node = document.getElementById("gif~"+i);
+                node.setAttribute("src",data.data[i].images.fixed_width.url);
+                // _trendingGIfs.appendChild(data);
             }
+        } else {
+            console.log('Reached GIPHY but API returned an error');
         }
-        request.send();
-    });
+    }
+    request.send();
+});
